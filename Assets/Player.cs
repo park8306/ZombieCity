02a7b0c53ed16472f335b4ef954bd7b32e394934
park8306 <kosaf0804@gmail.com>
2021-07-30 21:02:38 +0900

    플레이어 움직이기, 좀비 네비메쉬로 플레이어 따라가기

---
+++ Assets/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Animator animator;
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+    public float speed = 5;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) move.z = 1;
+        if (Input.GetKey(KeyCode.S)) move.z = -1;
+        if (Input.GetKey(KeyCode.D)) move.x = 1;
+        if (Input.GetKey(KeyCode.A)) move.x = -1;
+        if (move != Vector3.zero)
+        {
+            move.Normalize();
+            transform.Translate(move * speed * Time.deltaTime, Space.World);
+            transform.forward = move;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            animator.Play("Shoot");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            animator.Play("Run");
+        }
+    }
+}
