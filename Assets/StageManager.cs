743ef220c0913bd62bf78f62a7cedd0276f809d2
park8306 <kosaf0804@gmail.com>
2021-08-04 20:56:10 +0900

    UI 작업 중

---
+++ Assets/StageManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : SingletonMonoBehavior<StageManager>
+{
+    public SaveInt highScore;
+    public int score; // stageScore
+
+    new private void Awake()
+    {
+        base.Awake();
+        highScore = new SaveInt("hightScore");
+        score = 0;
+    }
+    public void AddScore(int addScore)
+    {
+        score += addScore;
+        if (highScore.Value < score)
+        {
+            highScore.Value = score;
+        }
+        UIRefresh();
+    }
+
+    private void UIRefresh()
+    {
+
+    }
+}
