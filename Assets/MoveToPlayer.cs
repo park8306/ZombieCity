bff9e6bdb5a14ff9470d175152c7759050870448
park8306 <kosaf0804@gmail.com>
2021-08-05 16:38:13 +0900

    코인의 목표지점 갱신

--- Assets/MoveToPlayer.cs
+++ Assets/MoveToPlayer.cs
@@ -9,12 +9,18 @@ public class MoveToPlayer : MonoBehaviour
     NavMeshAgent agent;
     public float maxSpeed = 20;
     public float duration = 3; // 3초동안 최대 20의 속도로 증가
-    private void OnTriggerEnter(Collider other)
+    private IEnumerator OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
             agent = GetComponent<NavMeshAgent>();
             DOTween.To(() => agent.speed, (x) => agent.speed = x, maxSpeed, duration);    // getter 초기값
+
+            while (true) // 코인의 목표지점을 계속 지정해줌
+            {
+                agent.destination = other.transform.position;
+                yield return null;
+            }
         }
     }
 }
